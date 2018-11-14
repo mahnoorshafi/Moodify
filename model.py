5c30df5b81f2f6879081b73d0b563d2121bdf17d
vagrant <vagrant@vagrant.vm>
2018-11-14 03:46:48 +0000

    Updated model to store users top artists

--- model.py
+++ model.py
@@ -5,33 +5,38 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    """ User of Adjacency """
+    """ User Information"""
 
     __tablename__ = 'users'
 
-    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    spotify_user_id = db.Column(db.Integer, nullable=False)
+    refresh_token = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
         return f"<User id={self.user_id}>"
 
-class Song(db.Model):
+class Track(db.Model):
     """ Song Information """
 
-    __tablename__ = 'songs'
+    __tablename__ = 'tracks'
 
-    song_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String(64), nullable=False)
-    artist = db.Column(db.String(64), nullable=False)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    spotify_track_id = db.Column(db.String, nullable=False)
+    track_name = db.Column(db.String, nullable=False)
+    artist_id = db.Column(db.String, db.ForeignKey('artists.id'), nullable=False)
     danceability = db.Column(db.Numeric(4,3), nullable=False)
     energy = db.Column(db.Numeric(4,3), nullable=False)
     valence = db.Column(db.Numeric(4,3), nullable=False)
 
+    artist = db.relationship('Artist', backref = 'tracks')
+
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return f"<Song id={self.song_id} title={self.title} artist={self.artist}>"
+        return f"<Track id={self.id} title={self.title} artist={self.artist}>"
 
 
 class Playlist(db.Model):
@@ -39,22 +44,60 @@ class Playlist(db.Model):
 
     __tablename__ = 'playlists'
 
-    playlist_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     mood = db.Column(db.Numeric(4,3), nullable=False)
 
-    user = db.relationship('User', backref = 'playlist')
-    songs = db.relationship('Song', secondary='playlist_song', backref = 'playlists')
+    user = db.relationship('User', backref = 'playlists')
+    tracks = db.relationship('Track', secondary='playlist_track')
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return f"<Playlist id={self.playlist_id} user={self.user_id} mood={self.mood}>"
+        return f"<Playlist id={self.id} user={self.user_id} mood={self.mood}>"
+
+class Artist(db.Model):
+    """ Artist Information """
+
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    artist_name = db.Column(db.String, nullable=False)
+    spotify_artist_id = db.Column(db.String, nullable=False)
 
 
-PlaylistSong = db.Table('playlist_song',
-        db.Column('playlist_id', db.Integer, db.ForeignKey('playlists.playlist_id'), nullable=False),
-        db.Column('song_id', db.Integer, db.ForeignKey('songs.song_id'), nullable=False)
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"<Artist id={self.id} name={self.name} spotify id={self.spotify_artist_id}>"
+
+
+class TopArtists(db.Model):
+    """ Users top artists information """
+
+    __tablename__ = 'top_artists'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship('User', backref = 'topartists')
+    artists = db.relationship('Artists', secondary='user_artists')
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"<Top Artists id={self.id} user={self.user_id}>"
+
+
+userArtists = db.Table('user_artists',
+        db.Column('user_id', db.Integer, db.ForeignKey('users.id'), nullable=False),
+        db.Column('artist_id', db.String, db.ForeignKey('artists.id'),nullable=False)
+    )
+
+
+playlistTrack = db.Table('playlist_track',
+        db.Column('playlist_id', db.Integer, db.ForeignKey('playlists.id'), nullable=False),
+        db.Column('track_id', db.Integer, db.ForeignKey('tracks.id'), nullable=False)
     )
 
 
