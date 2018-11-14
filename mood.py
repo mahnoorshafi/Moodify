a1edb05bd1a1682917549ecda0b26fe7fc73fd30
vagrant <vagrant@vagrant.vm>
2018-11-14 03:52:34 +0000

    select_tracks() added

--- mood.py
+++ mood.py
@@ -1,14 +1,15 @@
 import json
 import requests
+from random import shuffle
 from settings import *
 from pprint import pprint
 
-def get_users_top_artists(auth_header, num_entities = 20):
+def get_top_artists(auth_header, num_entities):
     """ Return list of user's top and followed artists """
 
     artists = []
 
-    term = ['short_term', 'medium_term', 'long_term']
+    term = ['long_term', 'short_term']
 
     for length in term:
         request = f'{SPOTIFY_API_URL}/me/top/artists?time_range={length}&limit={num_entities}'
@@ -31,11 +32,11 @@ def get_users_top_artists(auth_header, num_entities = 20):
 
 
 def get_related_artists(auth_header, top_artists):
-    """ Return list of related artists using users top 5 artists """
+    """ Return list of related artists using users top artist """
 
     new_artists = []
 
-    for artist_id in top_artists[:6]:
+    for artist_id in top_artists[:1]:
         request = f'{SPOTIFY_API_URL}/artists/{artist_id}/related-artists'
         related_artists_data = requests.get(request, headers=auth_header).json()
         related_artists = related_artists_data['artists']
@@ -59,17 +60,61 @@ def get_top_tracks(auth_header, artists):
         tracks = track_data['tracks']
 
         for track in tracks:
-            if (track['name'], track['id'], track['artists']) not in top_tracks:
-                top_tracks.append((track['name'], track['id'], track['artists']))
+            if track['id'] not in top_tracks:
+                top_tracks.append(track['id'])
 
     return top_tracks
 
+def cluster_ids(tracks, n = 100):
+    """ Return list of track ids clustered in groups of 100 """
+    
+    shuffle(tracks)
 
-def select_tracks(auth_header, tracks):
-    """ Select tracks based om user's mood """
+    clustered_tracks = []
+    for i in range(0, len(tracks), n):
+        clustered_tracks.append(tracks[i:i + n])
+
+    return clustered_tracks
+
+
+def select_tracks(auth_header, clustered_tracks, mood):
+    """ Select tracks based on user's mood """
 
     selected_tracks = []
 
+    for track_ids in clustered_tracks:
+        ids = '%2C'.join(track_ids)
+        request = f'{SPOTIFY_API_URL}/audio-features?ids={ids}'
+        audio_features_data = requests.get(request, headers=auth_header).json()
+        audio_features = audio_features_data['audio_features']
+
+    # Below is not selecting the correct tracks
+    #     for track in audio_features:
+    #         if mood <= 0.10:
+    #             if (track['danceability'] <= 0.10) and (track['energy'] <= 0.20) and (track['valence'] <= 0.10):
+    #                 selected_tracks.append(track['id'])
+    #         elif mood <= 0.25:
+    #             if (0.10 < track['danceability'] <= 0.25) and (0.20 < track['energy'] <= 0.30) and (0.10 < track['valence'] <= 0.20):
+    #                 selected_tracks.append(track['id'])
+    #         elif mood <= 0.50:
+    #             if (0.25 < track['danceability'] <= 0.05) and (0.30 < track['energy'] <= 0.40) and (0.20 < track['valence'] <= 0.40):
+    #                 selected_tracks.append(track['id'])
+    #         elif mood <= 0.75:
+    #             if (0.50 < track['danceability'] <= 0.75) and (0.40 < track['energy'] <= 0.50) and (0.50 < track['valence']):
+    #                 selected_tracks.append(track['id'])
+    #         elif mood <= 0.90:
+    #             if (0.75 < track['danceability'] <= 0.9) and (0.50 < track['energy'] <= 0.70) and (0.50 < track['valence']):
+    #                 selected_tracks.append(track['id'])
+    #         elif mood <= 1.00:
+    #             if (track['danceability'] > 0.9) and (track['energy'] > 0.7) and (0.50 < track['valence']):
+    #                 selected_tracks.append(track['id'])
+
+    # return selected_tracks
+
+def create_playlist(auth_header, selected_tracks):
+    """ Creates playlist based on mood """
+
+    pass
     
 
 
