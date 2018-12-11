2ec49d0cf7ed989023e39bf388f85500638c7d47
vagrant <vagrant@vagrant.vm>
2018-12-11 07:32:34 +0000

    Debug Mode Off

--- server.py
+++ server.py
@@ -8,5 +8,4 @@ if __name__ == '__main__':
     app.debug = False
     connect_to_db(app)
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-    DebugToolbarExtension(app)
     app.run(host = '0.0.0.0')
